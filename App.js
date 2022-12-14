624e778e016bbb3de28de326bc56a8d3f62c8b5f
Svetlana <svetakandral@yahoo.fr>
2022-12-14 10:46:45 +0100

    sveta_front

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import SigninScreen from "./screens/SigninScreen";
+import { requireNativeComponent } from "react-native";
 // import SignupScreen from "./screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
