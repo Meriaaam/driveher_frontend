e7823943713b760c63477326c8ace8f7c4431ed3
Svetlana <svetakandral@yahoo.fr>
2022-12-15 17:36:43 +0100

    profileScreen

--- App.js
+++ App.js
@@ -1,41 +1,41 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 // _________________SCREENS IMPORTS____________________________
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import HistoriqueScreen from './screens/HistoriqueScreen';
-import AccueilScreen from './screens/AccueilScreen';
-import OrderResumeScreen from './screens/OrderResumeScreen';
-import RatingScreen from './screens/RatingScreen';
-import YourDriverScreen from './screens/YourDriverScreen';
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import HistoriqueScreen from "./screens/HistoriqueScreen";
+import AccueilScreen from "./screens/AccueilScreen";
+import OrderResumeScreen from "./screens/OrderResumeScreen";
+import RatingScreen from "./screens/RatingScreen";
+import YourDriverScreen from "./screens/YourDriverScreen";
 
 // _________________REDUX IMPORTS_______________________________
 
-// redux 
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import user from './reducers/user';
+// redux
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import user from "./reducers/user";
 
-// redux-persist 
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import SigninScreen from './screens/SigninScreen';
-
+// redux-persist
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import SigninScreen from "./screens/SigninScreen";
 
 // ________________STORE CONFIGURATION_________________________
 const reducers = combineReducers({ user });
 const persistConfig = {
-  key: 'driveher',
+  key: "driveher",
   storage: AsyncStorage,
 };
 
 const store = configureStore({
   reducer: persistReducer(persistConfig, reducers),
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 const persistor = persistStore(store);
@@ -44,28 +44,29 @@ const persistor = persistStore(store);
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-
 // _______________Creation of the Tab Navigator____________________________
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName = '';
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
 
-        if(route.name === 'Accueil'){
-          iconName ='home'
-        }else if(route.name === 'Historique'){
-          iconName = 'history'
-        }else if(route.name === 'Profile'){
-          iconName = 'user'
-        }
+          if (route.name === "Accueil") {
+            iconName = "home";
+          } else if (route.name === "Historique") {
+            iconName = "history";
+          } else if (route.name === "Profile") {
+            iconName = "user";
+          }
 
-        return <FontAwesome name={iconName} size={30} color={color} />;
-      },
-      tabBarActiveTintColor: '#BE355C',
-      tabBarInactiveTintColor: '#b2b2b2',
-      headerShown: false,
-    })}>
+          return <FontAwesome name={iconName} size={30} color={color} />;
+        },
+        tabBarActiveTintColor: "#BE355C",
+        tabBarInactiveTintColor: "#b2b2b2",
+        headerShown: false,
+      })}
+    >
       <Tab.Screen name="Accueil" component={AccueilScreen} />
       <Tab.Screen name="Historique" component={HistoriqueScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
@@ -76,7 +77,7 @@ const TabNavigator = () => {
 export default function App() {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} >
+      <PersistGate persistor={persistor}>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Home" component={HomeScreen} />
--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,30 +1,159 @@
-import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native'
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import * as React from "react";
+import Header from "./Header";
+import { Avatar } from "react-native-paper";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
+import { useState } from "react";
 
-export default function ProfileScreen({navigation}) {
-    return (
-      <View style={styles.container}>
-          <Image style={styles.image} source={require('../assets/photo profile.png.png')} />
-          <Text>Profile</Text>
+export default function ProfileScreen({ navigation }) {
+  const user = useSelector((state) => state.user.value);
+
+  const [text, setText] = React.useState("");
+
+  const [focus, setFocus] = useState(false);
+  const [isEditable, setIsEditable] = useState(false);
+
+  const [firstName, setFirstName] = useState("");
+
+  
+useEffect(() => {
+
+  fetch(`https://driveher-backend.vercel.app/users/userData/${user.token}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("dataaa", data);
+
+      if (data.result) {
+        setFirstName(data.userData.firstName);
+        //         setPassword("");
+        //         // setIsModalVisible(false)
+      }
+    });
+},[])
+    // .catch((error) => console.log(error));
+  
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Avatar.Image
+        style={styles.avatar}
+        size={100}
+        source={require("../assets/photo_profile.png")}
+      />
+      <View style={styles.inputContainer}>
+        <TouchableOpacity onPress={() => setIsEditable(true)}>
+          <FontAwesome name="pencil" size={30} color="#BE355C" />
+          {/* <Text style={styles.textButton}>Modifier</Text> */}
+        </TouchableOpacity>
+
+        {/* <TouchableOpacity
         
+          // onPress={() => null}
+          // onFocus={() => this.setState({ bgColor: "green" })}
+          // onBlur={() => this.setState({ bgColor: "gray" })}
+        > */}
+        <TextInput
+          style={styles.input}
+          // placeholder="Prenom"
+          onChangeText={(value) => setFirstName(value)}
+          value={firstName} // onFocus={() => this.onFocus()}
+          editable={isEditable}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Nom"
+          // value={text}
+          editable={isEditable}
+          onChangeText={(text) => setText(text)}
+
+          // onPress={() => null}
+          // onFocus={() => this.setState({ bgColor: "green" })}
+          // onBlur={() => this.setState({ bgColor: "gray" })}
+        />
+        <TextInput
+          style={styles.input}
+          TextInput="Numero de telephone"
+          // value={text}
+          editable={isEditable}
+          onChangeText={(text) => setText(text)}
+        />
+        <TextInput
+          style={styles.input}
+          label="Email"
+          // value={text}
+          editable={isEditable}
+          onChangeText={(text) => setText(text)}
+        />
+        <TextInput
+          style={styles.input}
+          labeltext="Adresse favorite"
+          editable={isEditable}
+          onChangeText={(text) => setText(text)}
+        />
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => setIsEditable(false)}>
+            <FontAwesome name="rotate-left" size={30} color="#BE355C" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setIsEditable(false)}>
+            <FontAwesome name="check" size={30} color="#73DDAA" />
+          </TouchableOpacity>
+        </View>
+        {/* </TouchableOpacity> */}
       </View>
-    )
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor:'aqua'
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "aqua",
   },
 
-  image: {
-    width: '30%',
-    height: '30%',
-borderRadius: 50
+  inputContainer: {
+    width: "80%",
+    // flexDirection: "column",
+    borderBottomColor: "#BE355C",
+    //alignItems: "center",
+    // justifyContent: "center",
+    //     // backgroundColor: '#fbe29c',
   },
-})
+
+  input: {
+    width: "80%",
+    height: "10%",
+    marginTop: 5,
+    borderBottomColor: "#BE355C",
+    borderBottomWidth: 1,
+    fontSize: 18,
+  },
+
+  avatar: {
+    marginTop: 40,
+    marginBottom: 40,
+  },
+
+  buttonContainer: {
+    width: 300,
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+});
