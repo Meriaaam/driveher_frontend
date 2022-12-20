8ec85a70caddd2c620a63e64b37387cd895f8bcb
Svetlana <svetakandral@yahoo.fr>
2022-12-20 09:51:07 +0100

    rating

--- screens/RatingScreen.jsx
+++ screens/RatingScreen.jsx
@@ -1,26 +1,25 @@
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import {faStar} from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+// import {faStar} from '@fortawesome/free-solid-svg-icons';
 
 export default function RatingScreen({navigation}) {
   
-const Stars = () => {
-  const [rating, setRataing] = useState(0); //initial rating value 
+  // const element = <FontAwesomeIcon icon={faStar} />
+  // const [rating, setRataing] = useState(0); //initial rating value 
 
-  const stars =[];
-  for (let i = 0; i < 10; i++ ) {
-    let style = {'cursor': 'pointer'};
-    if (i < rating) {
-      style = { 'color': '#2196f3', 'cursor': 'pointer'  };
-    }
-    stars.push(<FontAwesomeIcon key={i} icon={faStar} onClick={()=> setRataing(i + 1)} style={style} />);
-  }
-  }
-  }
+  // const stars =[];
+  //  for (let i = 0; i < 10; i++ ) {
+  //    let style = {'cursor': 'pointer'};
+  //   if (i < rating) {
+  //     style = { 'color': '#2196f3', 'cursor': 'pointer'  };
+  //   }
+
 
     return (
       <View style={styles.container}>
+            {/* <FontAwesomeIcon key={i} icon={faStar} onClick={()=> setRataing(i + 1)} style={style} />; */}
+            {/* <FontAwesomeIcon icon={faStar} color="yellow" /> */}
           <Text>Rating</Text>
 
         
@@ -34,5 +33,5 @@ const styles = StyleSheet.create({
     alignItems:'center',
     justifyContent:'center',
     backgroundColor:'yellow'
-  }
-})
+  }});
+
